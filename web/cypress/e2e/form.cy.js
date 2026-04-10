bd8f9afe014fe2dc42b848e701070190f5e34dda
anajulia-v <anajuliavieira2408@gmail.com>
2026-04-10 17:14:27 -0300

    regression test

--- web/cypress/e2e/form.cy.js
+++ web/cypress/e2e/form.cy.js
@@ -1,30 +1,25 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
 
-    //login
-    cy.visit('http://localhost:3000/')
-    cy.get('#email').type('4dt@gmail.com')
-    cy.get('#password').type('4DT')
-    cy.contains('button', 'Entrar').click()
-
-    //form
-    cy.contains('h4', 'Formulários').click()
+  //TESTE preencher o form com sucesso
+  it('preencher o formulário com sucesso', () => {
+    cy.login('4dt@gmail.com', '4DT')
     
+    cy.contains('h4', 'Formulários').click()
+
     cy.get('#name').type('User User')
     cy.get('#email').type('user@gmail.com')
     cy.get('#phone').type('11 999996666').should('have.value', '(11) 99999-6666')
 
-    //tipo de consultoria - select 
+    //select 
     cy.get('#consultancyType').select('In Company')
 
-    //tipo de pessoa - radio
+    //radio
     cy.contains('span', 'Pessoa Jurídica').click()
     cy.contains('label', 'Pessoa Física').find('input').should('be.not.checked')
 
     cy.get('#document').type('12345678901234').should('have.value', '12.345.678/9012-34')
 
-    //onde - checkbox
+    //checkbox
     const redes = [
       'Instagram',
       'LinkedIn',
@@ -49,4 +44,33 @@ describe('template spec', () => {
     cy.contains('label', 'Li e aceito os termos de uso *').find('input').check().should('be.checked')
     cy.contains('button', 'Enviar formulário').click()
   })
+
+  //TESTE campos em branco
+  it('campos em branco', () => {
+    cy.login('4dt@gmail.com', '4DT')
+
+    cy.contains('h4', 'Formulários').click()
+
+    cy.get('#name')
+    cy.get('#email')
+    cy.contains('label', 'Li e aceito os termos de uso *').find('input')
+
+    cy.contains('button', 'Enviar formulário').click()
+
+    cy.contains('p', 'Campo obrigatório')
+    .should('be.visible')
+    .and('have.class', 'text-red-400')
+    .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    cy.contains('Campo obrigatório')
+    .should('be.visible')
+    .and('have.class', 'text-red-400')
+    .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    cy.contains('Você precisa aceitar os termos de uso')
+    .should('be.visible')
+    .and('have.class', 'text-red-400')
+    .and('have.css', 'color', 'rgb(248, 113, 113)')
+  })
+
 })
